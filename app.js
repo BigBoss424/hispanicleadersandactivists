7dd0ef0a7a8e744a98a296597cfc978fef1cd505
Aaron Jones <ajones061392@yahoo.com>
2022-09-23 16:59:27 -0700

    Added some lines of code to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// local leaders data 
+// local leaders data
 const leaders = [
 {
     id: 1,
@@ -16,20 +16,91 @@ const leaders = [
 },
 {
     id: 3,
-    name: "Cesar Chavez",
+    name: "Sonia Sotomayer",
     job: "Civil Right Leader",
     img: "#",
     text: "Cesar Chavez, born Cesario Estrada Chavez."
 },
 {
     id: 4,
-    name: "Cesar Chavez",
-    job: "Civil Right Leader"
+    name: "Roberto Clemente",
+    job: "Civil Right Leader",
     img: "#",
     text: "Cesar Chavez, born Cesario Estrada Chavez."
+},
+{
+    id: 5,
+    name: "Ellen Ochoa",
+    job: "#",
+    img: "#",
+    text: "Ellen Ochoa"
+},
+{
+    id: 6,
+    name: "Pablo Alvarado",
+    job: "#",
+    img: "#",
+    text: "Pablo Alvarado"
+},
+{
+    id: 7,
+    name: "Arturo Alfonso Schambrury",
+    job: "#",
+    img: "#",
+    text: "Arturo Alfonso Schambrury"
+},
+{
+    id: 8,
+    name: "Ellen Ochoa",
+    job: "#",
+    img: "#",
+    text: "Ellen Ochoa"
+},
+{
+    id: 9,
+    name: "Pancho Villa",
+    job: "#",
+    img: "#",
+    text: "Pancho Villa"
+},
+{
+    id: 10,
+    name: "Juan Felipe Herrera",
+    job: "#",
+    img: "#",
+    text: "Juan Felipe Herrera"
 }
 ];
 
+//select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+//set starting item
+let currentItem = 0;
+
+//load initial item
+window.addEventListener("DOMContentLoaded", function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person){
+    const item = leaders[person];
+    img.src = item.img;
+    author.textContent = item.job;
+    info.textContent = item.text;
+}
 //select items
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
@@ -43,7 +114,7 @@ const randomBtn =  document.querySelector(".random-btn");
 //set starting item
 let currentItem = 0;
 
-//load initial item 
+//load initial item
 window.addEventListener("DOMContentLoaded", function() {
     const item = leaders[currentItem];
     img.src = item.img;
